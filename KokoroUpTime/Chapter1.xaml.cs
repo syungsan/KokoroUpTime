c7e4127912d021483ed8566f6783250a67e22456
Popai0818 <64460648+Popai0818@users.noreply.github.com>
2020-06-22 16:41:17 +0900

    Update Chapter1.xaml.cs

--- KokoroUpTime/Chapter1.xaml.cs
+++ KokoroUpTime/Chapter1.xaml.cs
@@ -218,7 +218,7 @@ namespace KokoroUpTime
                 ["music_info_grid"] = this.MusicInfoGrid,
                 ["exit_back_grid"] = this.ExitBackGrid,
 
-                ["writing_grid"] = this.WritingGrid,
+               
             };
         }
 
@@ -317,8 +317,7 @@ namespace KokoroUpTime
 
            
 
-            this.WritingGrid.Visibility = Visibility.Hidden;
-            this.CanvasGrid.Visibility = Visibility.Hidden;
+           
 
             this.RuleBoardTitleTextBlock.Text = "";
             this.RuleBoardCheck1TextBlock.Text = "";
@@ -1038,82 +1037,6 @@ namespace KokoroUpTime
                 this.ExitBackGrid.Visibility = Visibility.Hidden;
                 this.CoverLayerImage.Visibility = Visibility.Hidden;
             }
-            if (button.Name == "NameButton")
-            {
-                this.CanvasGrid.Visibility = Visibility.Visible;
-                this.WritingGrid.Visibility = Visibility.Hidden;
-            }
-            if (button.Name == "PenButton")
-            {
-                WritingCanvas.EditingMode = InkCanvasEditingMode.Ink;
-            }
-            if (button.Name == "EraserButton")
-            {
-                WritingCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
-            }
-            if (button.Name == "AllEraseButton")
-            {
-                WritingCanvas.Strokes.Clear();
-            }
-            if (button.Name == "FinishButton")
-            {
-                if(WritingCanvas.Strokes != null)
-                {
-                    // ストロークが描画されている境界を取得
-                    Rect rectBounds = WritingCanvas.Strokes.GetBounds();
-
-                    // 描画先を作成
-                    DrawingVisual dv = new DrawingVisual();
-                    DrawingContext dc = dv.RenderOpen();
-                    // 描画エリアの位置補正（補正しないと黒い部分ができてしまう）
-                    dc.PushTransform(new TranslateTransform(-rectBounds.X, -rectBounds.Y));
-
-                    // 描画エリア(dc)に四角形を作成
-                    // 四角形の大きさはストロークが描画されている枠サイズとし、
-                    // 背景色はInkCanvasコントロールと同じにする
-                    dc.DrawRectangle(WritingCanvas.Background, null, rectBounds);
-
-                    // 上記で作成した描画エリア(dc)にInkCanvasのストロークを描画
-                    WritingCanvas.Strokes.Draw(dc);
-                    dc.Close();
-
-                    // ビジュアルオブジェクトをビットマップに変換する
-                    RenderTargetBitmap rtb = new RenderTargetBitmap(
-                        (int)rectBounds.Width, (int)rectBounds.Height,
-                        96, 96,
-                        PixelFormats.Default);
-                    rtb.Render(dv);
-
-                    // ビットマップエンコーダー変数の宣言
-                    BitmapEncoder enc = null;
-                    enc = new BmpBitmapEncoder();
-
-
-                    if (enc != null)
-                    {
-                        // ビットマップフレームを作成してエンコーダーにフレームを追加する
-                        enc.Frames.Add(BitmapFrame.Create(rtb));
-                        // ファイルに書き込む
-                        System.IO.Stream stream = System.IO.File.Create(@"C:\Users/maglab/Documents/Github/KoKoroUpTime/KoKoroUpTime/Log/Name.bmp");
-                        enc.Save(stream);
-                        stream.Close();
-                    }
-                    MessageBox.Show("start");
-                    this.NameText.Source= new BitmapImage(new Uri("/Log/Name.bmp", UriKind.RelativeOrAbsolute));
-                    MessageBox.Show("end");
-                }
-                this.CanvasGrid.Visibility = Visibility.Hidden;
-                this.WritingGrid.Visibility = Visibility.Visible;
-
-            }
-            if (button.Name =="DecisionButton")
-            {
-                this.WritingGrid.Visibility = Visibility.Hidden;
-
-                this.scenarioCount += 1;
-                this.ScenarioPlay();
-            }
-
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
