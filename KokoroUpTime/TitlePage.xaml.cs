cfa46280ae6fe7115f8d3b109d20b977350627f8
Popai0818 <64460648+Popai0818@users.noreply.github.com>
2020-07-03 13:08:09 +0900

    Revert "Update TitlePage.xaml.cs"
    This reverts commit 5e54f3aaa5cba91b7f3980909864e1a92d8d2884.

--- KokoroUpTime/TitlePage.xaml.cs
+++ KokoroUpTime/TitlePage.xaml.cs
@@ -13,13 +13,6 @@ using System.Windows.Shapes;
 
 using System.Reflection;
 using System.Diagnostics;
-using CsvReadWrite;
-using System.IO;
-using System.Windows.Media.TextFormatting;
-using SQLite;
-using System.Linq;
-using Microsoft.VisualBasic;
-using SQLitePCL;
 
 namespace KokoroUpTime
 {
@@ -31,13 +24,6 @@ namespace KokoroUpTime
         //全画面表示か
         private bool isMaximized = false;
 
-        public InitConfig initConfig = new InitConfig();
-        public DataOption dataOption = new DataOption();
-
-        private string[] dirPaths;
-
-        private bool isFirstBootFlag = true;
-
         public TitlePage()
         {
             InitializeComponent();
@@ -45,10 +31,6 @@ namespace KokoroUpTime
             // Hide host's navigation UI
             this.ShowsNavigationUI = false;
 
-            this.SelectUserListGrid.Visibility = Visibility.Hidden;
-            this.CoverLayerImage.Visibility = Visibility.Hidden;
-
-            // デバッグ用表示 #################################################################
             Assembly asm = Assembly.GetExecutingAssembly(); // 実行中のアセンブリを取得する。
 
             // AssemblyNameから取得
@@ -62,171 +44,6 @@ namespace KokoroUpTime
 
             this.VersionTextBlock.Text = name + version + fullname + processor + runtime + "\r\n";
             this.WindowTitle = asmName.Name + " Ver" + asmName.Version.ToString();
-            // ################################################################################
-        }
-
-        public void SetInitConfig(InitConfig _initConfig)
-        {
-            this.initConfig = _initConfig;
-        }
-
-        public void SetDataOption(DataOption _dataOption)
-        {
-            this.dataOption = _dataOption;
-        }
-
-        public void SetIsFirstBootFlag()
-        {
-            this.isFirstBootFlag = false;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (this.isFirstBootFlag)
-            {
-                this.LoadUser();
-            }
-            else
-            {
-                this.setCurrentUserName();
-            }
-        }
-
-        void LoadUser()
-        {
-            if (File.Exists("./Log/system.conf"))
-            {
-                using (var csv = new CsvReader("./Log/system.conf"))
-                {
-                    var csvs = csv.ReadToEnd();
-
-                    this.CurrentUserTextBlock.Text = $"{csvs[0][0]}{csvs[0][1]}";
-
-                    this.initConfig.userName = csvs[0][0];
-                    this.initConfig.userTitle = csvs[0][1];
-                }
-
-                string dbName = $"{this.initConfig.userName}.sqlite";
-                string userDirPath = $"./Log/{this.initConfig.userName}_{this.initConfig.userTitle}/";
-
-                var dbPath = System.IO.Path.Combine(userDirPath, dbName);
-
-                using (var connection = new SQLiteConnection(dbPath))
-                {
-                    var option = connection.Query<DataOption>("SELECT * FROM DataOption WHERE Id = 1;");
-
-                    foreach (var row in option)
-                    {
-                        this.dataOption.IsWordRecognition = row.IsWordRecognition;
-
-                        this.dataOption.IsPlaySE = row.IsPlaySE;
-
-                        this.dataOption.IsPlayBGM = row.IsPlayBGM;
-
-                        this.dataOption.MessageSpeed = row.MessageSpeed;
-
-                        this.dataOption.IsAddRubi = row.IsAddRubi;
-
-                        this.dataOption.CreatedAt = row.CreatedAt;
-                    }
-                }
-                this.setCurrentUserName();
-            }
-            else
-            {
-                this.CurrentUserTextBlock.Text = "名無しさん";
-            }
-
-            foreach (var confPath in new string[2] { $"./Log/{this.initConfig.userName}_{this.initConfig.userTitle}/user.conf", "./Log/system.conf" })
-            {
-                if (File.Exists(confPath))
-                {
-                    var accessTime = DateTime.Now.ToString();
-
-                    var initConfigs = new List<List<string>>();
-
-                    var initConfig = new List<string>() { this.initConfig.userName, this.initConfig.userTitle, accessTime };
-
-                    initConfigs.Add(initConfig);
-
-                    using (var csv = new CsvWriter(confPath))
-                    {
-                        csv.Write(initConfigs);
-                    }
-                }
-            }
-        }
-
-        void setCurrentUserName()
-        {
-            string userDirPath = $"./Log/{this.initConfig.userName}_{this.initConfig.userTitle}/";
-
-            if (!this.dataOption.IsWordRecognition)
-            {
-                // 実行ファイルの場所を絶対パスで取得
-                var startupPath = FileUtils.GetStartupPath();
-
-                this.CurrentNameImage.Source = new BitmapImage(new Uri($@"{startupPath}/{userDirPath}/Name.bmp", UriKind.Absolute));
-                this.CurrentUserTextBlock.Text = this.initConfig.userTitle;
-            }
-            else
-            {
-                this.CurrentNameImage.Source = null;
-                this.CurrentUserTextBlock.Text = $"{this.initConfig.userName}{this.initConfig.userTitle}";
-            }
-        }
-
-        void LoadUsers()
-        {
-            List<UserInfoItem> items = new List<UserInfoItem>();
-
-            this.dirPaths = Directory.GetDirectories("./Log/");
-
-            foreach (var dirPath in this.dirPaths)
-            {
-                if (File.Exists($"{dirPath}/user.conf"))
-                {
-                    string[] userInfo;
-
-                    bool isWordRecognition = false;
-
-                    using (var csv = new CsvReader($"{dirPath}/user.conf"))
-                    {
-                        var csvs = csv.ReadToEnd();
-                        userInfo = new string[3] { csvs[0][0], csvs[0][1], csvs[0][2] };
-                    }
-
-                    string dbName = $"{userInfo[0]}.sqlite";
-                    string dbDirPath = $"./Log/{userInfo[0]}_{userInfo[1]}/";
-
-                    var dbPath = System.IO.Path.Combine(dbDirPath, dbName);
-
-                    using (var connection = new SQLiteConnection(dbPath))
-                    {
-                        var option = connection.Query<DataOption>("SELECT IsWordRecognition FROM DataOption WHERE Id = 1;");
-
-                        foreach (var row in option)
-                        {
-                            isWordRecognition = row.IsWordRecognition;
-                        }
-                    }
- 
-                    if (!isWordRecognition)
-                    {
-                        var startupPath = FileUtils.GetStartupPath();
-                        items.Add(new UserInfoItem() { NameBmpPath = $@"{startupPath}/{dirPath}/Name.bmp", UserInfo = $"{userInfo[1]}, {userInfo[2]}" });
-                    }
-                    else
-                    {
-                        items.Add(new UserInfoItem() { NameBmpPath = null, UserInfo = $"{userInfo[0]}{userInfo[1]}, {userInfo[2]}" });
-                    }
-                }
-                else
-                {
-                    // error;
-                }
-            }
-            this.SelectUserListBox.ItemsSource = items;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -243,77 +60,43 @@ namespace KokoroUpTime
             {
                 OptionPage nextPage = new OptionPage();
 
-                nextPage.SetInitConfig(this.initConfig);
-                nextPage.SetDataOption(this.dataOption);
-
-                // this.NavigationService.Navigate(new Uri("OptionPage.xaml", UriKind.Relative));
+                this.NavigationService.Navigate(new Uri("OptionPage.xaml", UriKind.Relative));
 
                 this.NavigationService.Navigate(nextPage);
             }
-            else if (button.Content.ToString() == "なまえ入力")
+            else if (button.Content.ToString() == "なまえ")
             {
                 NameInputPage nextPage = new NameInputPage();
 
+                nextPage.SetScenario($"Scenarios/NameInput.csv");
+
                 this.NavigationService.Navigate(new Uri("NameInputPage.xaml", UriKind.Relative));
 
-                this.NavigationService.Navigate(nextPage);
-
-                nextPage.SetScenario("./Scenarios/nameinput.csv");
-            }
-            else if (button.Content.ToString() == "なまえ選択")
-            {
-                if (this.initConfig.userName == null)
-                {
-                    MessageBox.Show("なまえがひとつも登録されていません。\nまずは第1回から始めてください。");
-                }
-                else
-                {
-                    this.LoadUsers();
-
-                    this.CoverLayerImage.Visibility = Visibility.Visible;
-                    this.SelectUserListGrid.Visibility = Visibility.Visible;
-                }
-            }
-            else if (button.Content.ToString() == "OK")
-            {
-                var newUserNamePath = this.dirPaths[this.SelectUserListBox.SelectedIndex];
-
-                File.Copy($@"{newUserNamePath}/user.conf", @"./Log/system.conf", true);
-
-                this.LoadUser();
-
-                this.SelectUserListGrid.Visibility = Visibility.Hidden;
-                this.CoverLayerImage.Visibility = Visibility.Hidden;
+                // this.NavigationService.Navigate(nextPage);
             }
             else
             {
-                // Pageインスタンスを渡して遷移
-                switch (button.Content.ToString())
+                for (int i = 1; i <= 12; ++i)
                 {
                     // Pageインスタンスを渡して遷移
-    
-                    case "第1回":
+                    Chapter2 nextPage = new Chapter2();
 
-                        Chapter1 nextPage1 = new Chapter1();
+                    string scenario;
 
-                        nextPage1.SetInitConfig(this.initConfig);
-                        nextPage1.SetDataOption(this.dataOption);
+                    if (button.Content.ToString() == $"第{i}回")
+                    {
+                        scenario = $"Scenarios/chapter{i}.csv";
 
-                        nextPage1.SetScenario("./Scenarios/chapter1.csv");
-                        
-                        this.NavigationService.Navigate(nextPage1);
+                        nextPage.SetScenario(scenario);
 
-                        break;
+                        this.NavigationService.Navigate(new Uri($"Chapter{i}.xaml", UriKind.Relative));
 
-                    case "第2回":
-
-                        Chapter2 nextPage2 = new Chapter2();
-                        nextPage2.SetScenario("./Scenarios/chapter2.csv");
-
-                        this.NavigationService.Navigate(nextPage2);
+                        this.NavigationService.Navigate(nextPage);
 
                         break;
+                    }
                 }
+
             }
         }
 
@@ -345,11 +128,5 @@ namespace KokoroUpTime
                 isMaximized = false;
             }
         }
-    }
-
-    public class UserInfoItem
-    {
-        public string UserInfo { get; set; }
-        public string NameBmpPath { get; set; }
     }
 }
